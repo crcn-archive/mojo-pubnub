831955e2fff04acdf0d7331a401af585a4129ab2
Craig Condon <craig@spiceapps.com>
2014-09-29 16:37:41 -0700

    initial commit for realtime system

--- examples/todos/app/index.js
+++ examples/todos/app/index.js
@@ -4,6 +4,12 @@ window.$ = require("jquery");
 
 
 var TodosApplication = Application.extend({
+  config: {
+    pubnub: {
+      subscribe_key: "sub-c-9535ac96-4823-11e4-aaa5-02ee2ddab7fe",
+      publish_key: "pub-c-55ed0f2b-6b2e-455a-977e-9530bb287d2f"
+    }
+  },
   plugins: [
     require("mojo-views"),
     require("mojo-paperclip"),
@@ -18,6 +24,10 @@ var TodosApplication = Application.extend({
 var app = new TodosApplication();
 
 $(document).ready(function () {
+
+  console.log("PUB");
+  app.eventBus.publish("/channel", "todos");
+
   document.body.appendChild(app.views.create("main", {
     todos: app.models.create("todos")
   }).render());
